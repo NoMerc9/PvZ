f53a3e2b63ba37543ed176e2d11052b7107a989e
NoMerc9 <filoxion545433@gmail.com>
2026-09-14 03:33:39 +0800

    Wallnut.cpp

--- Wallnut.cpp
+++ Wallnut.cpp
@@ -4,7 +4,4 @@
 Wallnut::Wallnut(sf::Vector2f pos)
     : Plant(pos, 400, 25, 999.0f, "assets/wallnut.png")
 {
-    // cooldownMax = 999 — орех никогда не «стреляет»
-    // hp = 400 — в 4 раза больше обычного растения
-    // cost = 50 — как подсолнух
 }
